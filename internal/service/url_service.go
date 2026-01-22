5a5cbf263d7dd7328995bd5596a8aa13ad8c179f
Minvydas <minderrx2@gmail.com>
2026-01-22 10:00:33 +0000

    added logger per request

--- internal/service/url_service.go
+++ internal/service/url_service.go
@@ -3,6 +3,7 @@ package service
 import (
 	"context"
 	"errors"
+	"log/slog"
 	"math/rand"
 	"time"
 
@@ -12,34 +13,47 @@ import (
 const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 type URLService struct {
-	store domain.URLStore
-	cache domain.URLCache
+	store  domain.URLStore
+	cache  domain.URLCache
+	logger *slog.Logger
 }
 
-func NewURLService(store domain.URLStore, cache domain.URLCache) *URLService {
+func NewURLService(store domain.URLStore, cache domain.URLCache, logger *slog.Logger) *URLService {
 	return &URLService{
-		store: store,
-		cache: cache,
+		store:  store,
+		cache:  cache,
+		logger: logger,
 	}
 }
 
 func (s *URLService) Shorten(ctx context.Context, originalURL string) (*domain.URL, error) {
-	ShortURL := generateShortURL(6)
+	var url *domain.URL
+	var shortURL string
+	var success bool
+	for i := 0; i < 3; i++ {
+		shortURL = generateShortURL(6)
 
-	url := &domain.URL{
-		OriginalURL: originalURL,
-		ShortURL:    ShortURL,
-		CreatedAt:   time.Now(),
+		url = &domain.URL{
+			OriginalURL: originalURL,
+			ShortURL:    shortURL,
+			CreatedAt:   time.Now(),
+		}
+
+		if err := s.store.CreateURL(ctx, url); errors.Is(err, domain.ErrCollision) {
+			continue
+		} else if err != nil {
+			return nil, err
+		}
+		success = true
+		break
 	}
-
-	if err := s.store.CreateURL(ctx, url); err != nil {
-		return nil, err
+	if !success {
+		return nil, errors.New("Failed to shorten URL")
 	}
+	if err := s.cache.Set(ctx, shortURL, url); err != nil {
+		s.logger.Error("Failed to set cache", "error", err)
 
-	if err := s.cache.Set(ctx, ShortURL, url); err != nil {
-		// Add logging later
 	}
-
 	return url, nil
 }
 
@@ -53,7 +67,7 @@ func (s *URLService) Resolve(ctx context.Context, code string) (*domain.URL, err
 	// Slow database lookup
 	url, err = s.store.GetByShortURL(ctx, code)
 	if err != nil {
-		return nil, errors.New("url not found")
+		return nil, errors.New("URL not found")
 	}
 
 	// Add to cache for next time
@@ -63,6 +77,7 @@ func (s *URLService) Resolve(ctx context.Context, code string) (*domain.URL, err
 }
 
 // Takes random chars from charset
+// Future convert to base62
 func generateShortURL(length int) string {
 	b := make([]byte, length)
 	for i := range b {
