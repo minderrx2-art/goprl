3226cc9a054dd09c3500169360823a26fbb341c6
Minvydas <minderrx2@gmail.com>
2026-01-26 14:24:40 +0000

    declutter main.go

--- main.go
+++ main.go
@@ -3,97 +3,30 @@ package main
 import (
 	"fmt"
 	"goprl/internal/api"
-	"goprl/internal/service"
-	"goprl/internal/store"
-	"goprl/internal/store/postgres"
-	"goprl/internal/store/redis"
-
-	"log/slog"
+	"goprl/internal/app"
+	"goprl/internal/config"
 	"net/http"
-	"os"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	app := buildApp()
-	defer app.close()
+	config, err := config.NewConfig()
+	if err != nil {
+		panic("CONFIG: " + err.Error())
+	}
+	app, err := app.NewApp(config)
+	if err != nil {
+		panic("APP: " + err.Error())
+	}
+	defer app.Close()
 
 	mux := http.NewServeMux()
-	app.handler.RegisterRoutes(mux)
+	app.Handler.RegisterRoutes(mux)
 
-	fmt.Println("URL Shortener starting on http://localhost:" + app.config.port)
-	if err := http.ListenAndServe(":"+app.config.port,
+	fmt.Println("URL Shortener starting on http://localhost:" + app.Config.Port)
+	if err := http.ListenAndServe(":"+app.Config.Port,
 		api.RequestIDMiddleware(
-			api.LoggingMiddleware(app.logger)(mux),
+			api.LoggingMiddleware(app.Logger)(mux),
 		)); err != nil {
-		panic(err)
+		panic("SERVER: " + err.Error())
 	}
-}
-
-type config struct {
-	databaseURL string
-	redisURL    string
-	port        string
-}
-
-func LoadConfig() (*config, error) {
-	_ = godotenv.Load()
-	var databaseURL, redisURL, port string
-	if port = os.Getenv("PORT"); port == "" {
-		port = "8080"
-	}
-	if databaseURL = os.Getenv("DATABASE_URL"); databaseURL == "" {
-		return nil, fmt.Errorf("DATABASE_URL is not set")
-	}
-	if redisURL = os.Getenv("REDIS_URL"); redisURL == "" {
-		return nil, fmt.Errorf("REDIS_URL is not set")
-	}
-	return &config{
-		databaseURL: databaseURL,
-		redisURL:    redisURL,
-		port:        port,
-	}, nil
-}
-
-type app struct {
-	postgresStore *postgres.Store
-	redisStore    *redis.Cache
-	logger        *slog.Logger
-	handler       *api.Handler
-	config        *config
-}
-
-func buildApp() *app {
-	config, err := LoadConfig()
-	if err != nil {
-		panic("ENV: " + err.Error())
-	}
-
-	postgresStore, err := store.NewPostgresStore(config.databaseURL)
-	if err != nil {
-		panic("POSTGRES: " + err.Error())
-	}
-
-	redisStore, err := store.NewRedisStore(config.redisURL)
-	if err != nil {
-		panic("REDIS: " + err.Error())
-	}
-
-	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
-	service := service.NewURLService(postgresStore, redisStore, logger)
-	handler := api.NewHandler(service)
-
-	return &app{
-		postgresStore: postgresStore,
-		redisStore:    redisStore,
-		logger:        logger,
-		handler:       handler,
-		config:        config,
-	}
-}
-
-func (a *app) close() {
-	a.postgresStore.Close()
-	a.redisStore.Close()
 }
